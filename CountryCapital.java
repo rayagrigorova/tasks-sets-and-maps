954a01fe583b0b5cafeda06e60e83e3ae61e7e5b
raiagrigorova2 <72023155+raiagrigorova2@users.noreply.github.com>
2021-03-29 22:56:34 +0300

    10. Задачи

--- CountryCapital.java
+++ CountryCapital.java
@@ -7,41 +7,29 @@ import java.util.*;
 public class CountryCapital {
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
-//I will use a hash map because it accesses elements quickly 
+		//I will use a hash map because it accesses elements quickly 	
 		Map <String, String> countryCapital = new HashMap<String, String>();
-		ArrayList <String[]> countryCapitalList = new ArrayList <>();
-		
+
 		//Read the names of capitals and countries from file 
 		File file = new File("src/SetsAndMaps/capitalCountry");
 		Scanner input = new Scanner(file);
 		while(input.hasNext()) {
 			String s = input.nextLine();
 			String [] cc = s.split("-");
-			countryCapitalList.add(cc);		
+			countryCapital.put(cc[1], cc[0]);		
 		}
-		
-		int listSize = countryCapitalList.size();
+		List<String> countryCapitalList = new ArrayList<>(countryCapital.keySet());
+
 		int points = 0;
-		while(true){
-			countryCapital.clear();
-			
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			Collections.shuffle(countryCapitalList);
 			for(int i = 0; i < 5; i++) {
-				Random r = new Random();
-				int randomInd = r.nextInt(listSize);
-				String randomCountry = countryCapitalList.get(randomInd)[1];
-				String randomCapital = countryCapitalList.get(randomInd)[0];
-				
-				countryCapital.put(randomCountry, randomCapital);
-			}
-			input.close();
+				String val = countryCapital.get(countryCapitalList.get(i));
 			
-			Scanner sc = new Scanner(System.in);
-			Set <Map.Entry<String, String>> entrySet = countryCapital.entrySet();
-			
-			for(Map.Entry<String, String> entry : entrySet ) {
-				System.out.println("What's the capital of " + entry.getKey().trim() + "?");
+				System.out.println("What's the capital of " + countryCapitalList.get(i).trim() + "?");
 				String ans = sc.nextLine();
-				if(ans.equalsIgnoreCase(entry.getValue().trim())) {
+				if(ans.equalsIgnoreCase(val.trim())) {
 					System.out.println("Correct!");
 					points++;
 				}
@@ -50,6 +38,7 @@ public class CountryCapital {
 					points--;
 				}
 			}
+			
 			System.out.println("Points: " + points);
 			
 			System.out.println("Do you want to play again?(Yes/No)");
@@ -61,6 +50,7 @@ public class CountryCapital {
 			default: System.out.println("Invalid input");
 			}
 			if(!playAgain) break;
+			else sc.nextLine();
 		}
 
 	}
--- CountryCapital_2.java
+++ CountryCapital_2.java
@@ -3,7 +3,9 @@ package SetsAndMaps;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,50 +14,37 @@ import java.util.Set;
 public class CountryCapital_2 {
 	//some countries have more than one capital (for example South Africa) So all the capitals a country has must be correct answers 
 	public static void main(String[] args) throws FileNotFoundException {
-		Map <String, String> capitalCountry = new HashMap<String, String>();
-		ArrayList <String[]> countryCapitalList = new ArrayList <>();
+		Map <String, String> countryCapital = new HashMap<String, String>();
+
 		//Read the names of capitals and countries from file 
 		File file = new File("src/SetsAndMaps/capitalCountry");
 		Scanner input = new Scanner(file);
 		while(input.hasNext()) {
 			String s = input.nextLine();
 			String [] cc = s.split("-");
-			countryCapitalList.add(cc);
-			
+			countryCapital.put(cc[0], cc[1]);		
 		}
-		
-		int listSize = countryCapitalList.size();
+		List<String> countryCapitalList = new ArrayList<>(countryCapital.keySet());
+
 		int points = 0;
-		while(true) {
-			capitalCountry.clear();
+		Scanner sc = new Scanner(System.in);
+		while(true) {		
+			Collections.shuffle(countryCapitalList);
 			for(int i = 0; i < 5; i++) {
-				Random r = new Random();
-				int randomInd = r.nextInt(listSize);
-				String randomCountry = countryCapitalList.get(randomInd)[1];
-				String randomCapital = countryCapitalList.get(randomInd)[0];
-				
-				capitalCountry.put(randomCapital,randomCountry);
-			}
+				String val = countryCapital.get(countryCapitalList.get(i));
 			
-			input.close();
-			
-			Scanner sc = new Scanner(System.in);
-			Set <Map.Entry<String, String>> entrySet = capitalCountry.entrySet();
-			
-			for(Map.Entry<String, String> entry : entrySet ) {
-				System.out.println("What's the capital of " + entry.getValue().trim() + "?");
+				System.out.println("What's the capital of " + val.trim() + "?");
 				String ans = sc.nextLine();
-				if(capitalCountry.get(ans) != null) {
-					if(capitalCountry.get(ans).trim().equals(entry.getValue().trim())) {
-						System.out.println("Correct!");
-						points++;
-					}
+				if(ans.equalsIgnoreCase(countryCapitalList.get(i).trim())) {
+					System.out.println("Correct!");
+					points++;
 				}
 				else {
 					System.out.println("Sorry, wrong answer.");
 					points--;
 				}
 			}
+			
 			System.out.println("Points: " + points);
 			
 			System.out.println("Do you want to play again?(Yes/No)");
@@ -67,8 +56,7 @@ public class CountryCapital_2 {
 			default: System.out.println("Invalid input");
 			}
 			if(!playAgain) break;
-			
-		
+			else sc.nextLine();
 		}
 	}
 }
